218ca9c86400f486afcf150e3b6881b56b64b274
Nadejda <nadyash295923@gmail.com>
2024-04-11 19:31:49 +0300

    done CP

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        if (x == 0 || y == 0) System.out.println("zero");
+        if (x>0)
+        {
+            if (y > 0) System.out.println("first");
+            if (y < 0) System.out.println("forth");
+        }
+        if (x < 0)
+        {
+            if (y > 0) System.out.println("second");
+            if (y < 0) System.out.println("third");
+        }
     }
 }
